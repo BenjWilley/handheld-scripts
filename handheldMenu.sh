6a1d0d807cb13a680128b33a36b8392fbbd55a33
benjwilley <ben@wwilley.com>
2024-03-05 12:56:23 -0600

    updated pins

--- handheldMenu.sh
+++ handheldMenu.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+#enable GPS
+
+
 #set canned message inputs/events
 meshtastic --set canned_message.enabled true 
-meshtastic --set canned_message.inputbroker_pin_a 4
-meshtastic --set canned_message.inputbroker_pin_b 10
-meshtastic --set canned_message.inputbroker_pin_press 9
+meshtastic --set canned_message.inputbroker_pin_a 16
+meshtastic --set canned_message.inputbroker_pin_b 17
+meshtastic --set canned_message.inputbroker_pin_press 18
 meshtastic --set canned_message.inputbroker_event_cw Down
 meshtastic --set canned_message.inputbroker_event_ccw Up
 meshtastic --set canned_message.inputbroker_event_press Select
@@ -21,7 +24,7 @@ meshtastic --set detection_sensor.state_broadcast_secs 1000
 
 #SOS sensor name
 meshtastic --set detection_sensor.name "Hiker SOS"
-meshtastic --set detection_sensor.monitor_pin 7
+meshtastic --set detection_sensor.monitor_pin 15
 meshtastic --set detection_sensor.detection_triggered_high true
 
 
