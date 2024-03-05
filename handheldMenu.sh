41313fff32c0c8d37de0fbb367d11e7cff0a82b2
benjwilley <ben@wwilley.com>
2024-03-05 13:00:14 -0600

    changed gpio

--- handheldMenu.sh
+++ handheldMenu.sh
@@ -5,9 +5,9 @@
 
 #set canned message inputs/events
 meshtastic --set canned_message.enabled true 
-meshtastic --set canned_message.inputbroker_pin_a 16
-meshtastic --set canned_message.inputbroker_pin_b 17
-meshtastic --set canned_message.inputbroker_pin_press 18
+meshtastic --set canned_message.inputbroker_pin_a 3
+meshtastic --set canned_message.inputbroker_pin_b 2
+meshtastic --set canned_message.inputbroker_pin_press 1
 meshtastic --set canned_message.inputbroker_event_cw Down
 meshtastic --set canned_message.inputbroker_event_ccw Up
 meshtastic --set canned_message.inputbroker_event_press Select
@@ -24,7 +24,7 @@ meshtastic --set detection_sensor.state_broadcast_secs 1000
 
 #SOS sensor name
 meshtastic --set detection_sensor.name "Hiker SOS"
-meshtastic --set detection_sensor.monitor_pin 15
+meshtastic --set detection_sensor.monitor_pin 4
 meshtastic --set detection_sensor.detection_triggered_high true
 
 
